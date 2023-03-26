978b0dfe28897100148f6fe0705381c97b991d5a
Alek Novkovski <alek@greatcode2.com>
2023-03-26 23:10:16 +0200

    Add email validator, just alerts if invalid

--- working/index.js
+++ working/index.js
@@ -1,1 +1,15 @@
-alert("javascript added")
+const formEl = document.querySelector(".signup-form")
+const inputEl = document.querySelector(".signup-form input")
+
+const validateEmail = (email) => {
+    const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    return email.match(pattern);
+};
+
+formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const emailIsValid = validateEmail(inputEl.value);
+    if(!emailIsValid) {
+        alert("email not valid")
+    }
+})
